d17624fb79fd87c62050ddd7fd6c7eacee00e2ea
ced <zuger.cedric@gmail.com>
2018-03-27 13:48:10 +0200

    Linked db.

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :rbenv_ruby, '2.4.2'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml', 'db/production.sqlite3'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
