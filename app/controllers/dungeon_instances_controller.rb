83681284af51bc51e7a537985c01f58248381ffb
ced <zuger.cedric@gmail.com>
2018-04-11 12:20:38 +0200

    Patch issue fixed.

--- app/controllers/dungeon_instances_controller.rb
+++ app/controllers/dungeon_instances_controller.rb
@@ -1,7 +1,7 @@
 require 'dungeon'
 
 class DungeonInstancesController < ApplicationController
-  before_action :set_dungeon_instance, only: [:show, :play, :edit, :update, :clear_room]
+  before_action :set_dungeon_instance, only: [:show, :play, :edit, :move, :clear_room]
 
   DIFFICULTY = [:easy, :medium, :hard, :deadly]
 
